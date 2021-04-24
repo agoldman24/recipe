0f4f5e59d4b8530dc74275d303df86145691293c
Unknown <abg@vt.edu>
2021-04-24 12:54:17 -0400

    installed prettier, formatted all files in codebase

--- server/secret.js
+++ server/secret.js
@@ -1,6 +1,7 @@
 const secrets = {
-  dbUri: "mongodb+srv://admin:Goldilocks493!@cluster0-7wzdn.mongodb.net/recipebook?retryWrites=true&w=majority"
+  dbUri:
+    "mongodb+srv://admin:Goldilocks493!@cluster0-7wzdn.mongodb.net/recipebook?retryWrites=true&w=majority",
 };
-const getSecret = key => secrets[key];
+const getSecret = (key) => secrets[key];
 
 module.exports = getSecret;
